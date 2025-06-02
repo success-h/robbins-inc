b6e30730dde0ebe5b5fadfc6eeb83209c5bfb771
success-h <successhycenth8@gmail.com>
2025-06-02 11:38:27 +0100

    update

--- next.config.ts
+++ next.config.ts
@@ -11,15 +11,15 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  // async redirects() {
-  //   return [
-  //     {
-  //       source: '/',
-  //       destination: '/en',
-  //       permanent: true,
-  //     },
-  //   ];
-  // },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/en',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
